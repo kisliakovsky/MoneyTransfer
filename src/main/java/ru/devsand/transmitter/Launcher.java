7212ecc8e21481f6c5b8a80e02f2a1933a3af2bd
kisliakovsky <kisliakovskiii@yandex.ru>
2018-05-31 11:49:54 +0300

    Main class renamed.

--- src/main/java/ru/devsand/transmitter/Launcher.java
+++ src/main/java/ru/devsand/transmitter/Launcher.java
@@ -1,6 +1,6 @@
 package ru.devsand.transmitter;
 
-public class Main {
+public class Launcher {
 
     public static void main(String[] args) {
 
--- src/test/java/ru/devsand/transmitter/LauncherTest.java
+++ src/test/java/ru/devsand/transmitter/LauncherTest.java
@@ -4,6 +4,6 @@ import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class MainTest {
+public class LauncherTest {
 
 }
