14bd0d6b7012ec685d4e683fe74a18158ba41af3
kisliakovsky <kisliakovskiii@yandex.ru>
2018-06-06 14:01:56 +0300

    On start errors directed to stderr.

--- src/main/java/ru/devsand/transmitter/Launcher.java
+++ src/main/java/ru/devsand/transmitter/Launcher.java
@@ -1,23 +1,16 @@
 package ru.devsand.transmitter;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import ru.devsand.transmitter.model.service.UnableSaveException;
 import ru.devsand.transmitter.rest.BasicRestService;
 import ru.devsand.transmitter.rest.RestService;
 
-import java.sql.SQLException;
-
 public class Launcher {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(Launcher.class.getName());
 
     public static void main(String[] args) {
         try {
             RestService restService = new BasicRestService(4567);
             restService.start();
         } catch (Exception e) {
-            LOGGER.error("Unable to start REST service", e);
+            System.err.println("Unable to start REST service");
         }
     }
 
